4ad3c636590e21f1aa4871f480b6841c7f2ced5b
celalcolak94 <celalcolak94@gmail.com>
2023-07-22 11:29:14 +0300

    Bcrypt Password Hash

--- MVCApp/Controllers/AccountController.cs
+++ MVCApp/Controllers/AccountController.cs
@@ -21,43 +21,60 @@ namespace MVCApp.Controllers
         [HttpPost]
         public IActionResult Login(LoginModel model)
         {
-            var db = new TestDbContext();
-
-            //User rolleri joinle sonra user rolleri ile birlikte dbden bul
-            var user = db.Users.Include(x => x.Roles).FirstOrDefault(x => x.UserName == model.UserName && x.Password == model.Password);
-
-
-            if (user != null)
+            if (ModelState.IsValid)
             {
-                List<Claim> claims = new List<Claim>(); //Login olurken sistemde cookiede saklanacak kullanıcı değerlerini listeye atacağımız liste
+                var db = new TestDbContext();
 
-                claims.Add(new Claim(ClaimTypes.Name, user.UserName));
-                claims.Add(new Claim(ClaimTypes.Email, user.Email));
+                //User rolleri joinle sonra user rolleri ile birlikte dbden bul
+                var user = db.Users.Include(x => x.Roles).FirstOrDefault(x => x.UserName == model.UserName);
 
-                foreach (var role in user.Roles)
+                if (user != null)
                 {
-                    claims.Add(new Claim(ClaimTypes.Role, role.Name));
+                    var hashPass = BCrypt.Net.BCrypt.HashPassword(model.Password, user.PasswordSalt);
+
+                    if (user.PasswordHash == hashPass)
+                    {
+                        List<Claim> claims = new List<Claim>(); //Login olurken sistemde cookiede saklanacak kullanıcı değerlerini listeye atacağımız liste
+
+                        claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                        claims.Add(new Claim(ClaimTypes.Email, user.Email));
+
+                        foreach (var role in user.Roles)
+                        {
+                            claims.Add(new Claim(ClaimTypes.Role, role.Name));
+                        }
+
+
+                        var identity = new ClaimsIdentity(claims, "YZL3439"); //Login olacak kişi için yukarıdaki özelliklerde bir kimlik oluşturduk.
+                        var claimPrinciple = new ClaimsPrincipal(identity); //Bu kimlik bilgilerini sisteme oturum açacak olan sınıfa atamasını yaptık.
+
+
+
+                        var authProps = new AuthenticationProperties(); //Yani kimlik doğrulama oluşacak olan cookie kalıcı olup olmayacağı gibi bilgileri bu sınıf üzerinden belirleriz.
+                        authProps.IsPersistent = model.RememberMe; //Cookie kalıcı olsun.        Session bazlı olmasın demek.
+
+                        if (model.RememberMe)
+                        {
+                            authProps.ExpiresUtc = DateTimeOffset.UtcNow.AddDays(30);
+                        }
+                        //Eğer kullanıcı beni hatırlayı seçtiyse 1 aylık cookie
+
+                        HttpContext.SignInAsync(claimPrinciple, authProps); //Sistemde cookie oluşturmamızı sağlayacak kod.
+
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Password", "Parola doğru değil.");
+                        return View(model);
+                    }
                 }
-
-
-                var identity = new ClaimsIdentity(claims, "YZL3439"); //Login olacak kişi için yukarıdaki özelliklerde bir kimlik oluşturduk.
-                var claimPrinciple = new ClaimsPrincipal(identity); //Bu kimlik bilgilerini sisteme oturum açacak olan sınıfa atamasını yaptık.
-
-
-
-                var authProps = new AuthenticationProperties(); //Yani kimlik doğrulama oluşacak olan cookie kalıcı olup olmayacağı gibi bilgileri bu sınıf üzerinden belirleriz.
-                authProps.IsPersistent = model.RememberMe; //Cookie kalıcı olsun.        Session bazlı olmasın demek.
-
-                if (model.RememberMe)
+                else
                 {
-                    authProps.ExpiresUtc = DateTimeOffset.UtcNow.AddDays(30);
+                    ModelState.AddModelError("UserName", "Kullanıcı Adı mevcut değil.");
+                    return View(model);
                 }
-                //Eğer kullanıcı beni hatırlayı seçtiyse 1 aylık cookie
-
-                HttpContext.SignInAsync(claimPrinciple, authProps); //Sistemde cookie oluşturmamızı sağlayacak kod.
-
-                return RedirectToAction("Index", "Home");
-
             }
             else
             {
@@ -91,7 +108,14 @@ namespace MVCApp.Controllers
                     var user = new User();
                     user.Email = model.Email;
                     user.UserName = model.UserName;
-                    user.Password = model.Password;
+
+                    string salt = BCrypt.Net.BCrypt.GenerateSalt();//Kendin salt değeri oluştur.)
+                    string passHash = BCrypt.Net.BCrypt.HashPassword(model.Password, salt: salt);
+                    user.PasswordHash = passHash;
+                    user.PasswordSalt = salt;
+
+                    //user.PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(model.Password, BCrypt.Net.HashType.SHA512);
+                    //string normal = BCrypt.Net.BCrypt.HashPassword(model.Password);
 
                     var db = new TestDbContext();
                     db.Users.Add(user);
--- MVCApp/Data/User.cs
+++ MVCApp/Data/User.cs
@@ -5,7 +5,8 @@
         public string Id { get; init; } //Sadece consturctordan init olsun diye bir özellik
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? PasswordSalt { get; set; }
 
         //Kullanıcının Rolleri
         public List<Role> Roles { get; set; }
